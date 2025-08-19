357de982ccb294189ee571802accd3d654bb750d
Daniel Felix <danielfelix1995@gmail.com>
2025-08-19 19:03:26 +0530

    -- added few more extensions to php7.4

--- setup.sh
+++ setup.sh
@@ -46,7 +46,7 @@ echo "* Installing PHP 7.4..."
 sudo apt-get install -y php7.4 php7.4-common php7.4-cli > /dev/null
 
 echo "* Installing PHP 7.4 extensions..."
-sudo apt-get install -y php7.4-bz2 php7.4-curl php7.4-gd php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-soap php7.4-sqlite3 php7.4-tidy php7.4-xml php7.4-xsl php7.4-zip > /dev/null
+sudo apt-get install -y php7.4-bz2 php7.4-curl php7.4-gd php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-soap php7.4-sqlite3 php7.4-tidy php7.4-xml php7.4-xsl php7.4-zip php7.4-apcu php7.4-gearman php7.4-imagick php7.4-intl php7.4-mcrypt > /dev/null
 
 echo "* Installing PHP 8.0..."
 sudo apt-get install -y php8.0 php8.0-common php8.0-cli > /dev/null
